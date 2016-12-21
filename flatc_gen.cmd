1d098d2eadf436b65792989f5214a86c14fa483d
kenkao <kenkao_01@126.com>
2016-12-21 17:31:33 +0800

    1

--- flatc_gen.cmd
+++ flatc_gen.cmd
@@ -1,4 +1,4 @@
-flatc -n -o E:/GoLang/GoPath/idl/net/ E:/GoLang/GoPath/idl/src/test.idl
-flatc -g -o E:/GoLang/GoPath/idl/go/ E:/GoLang/GoPath/idl/src/test.idl
+flatc -n -o E:/GoLang/GoPath/idl/net/ E:/GoLang/GoPath/idl/src/my.idl
+flatc -g -o E:/GoLang/GoPath/idl/go/ E:/GoLang/GoPath/idl/src/my.idl
 flatc -n -o E:/GoLang/GoPath/idl/net/ E:/GoLang/GoPath/idl/src/test.idl
 flatc -g -o E:/GoLang/GoPath/idl/go/ E:/GoLang/GoPath/idl/src/test.idl
